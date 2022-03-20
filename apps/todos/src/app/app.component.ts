eb4fd543c2b77a7953d570726493d6748ecd4389
Anderson E. Peñaloza C <designeromega@gmail.com>
2022-03-20 17:08:25 +0100

    add base todo front and nest plgugin

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface Todos {
+  title: string;
+}
 @Component({
   selector: 'my-org-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
+
+  todos: Todos[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http
+      .get<Todos[]>('/api/todos')
+      .subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/todos', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
